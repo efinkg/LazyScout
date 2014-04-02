a5a58b9aa01cf113551494ee5345e356577a0370
efinkg <efinkg@gmail.com>
2014-04-02 18:37:19 -0500

    Making a proper lazy scout
    This script will find teams you are still going to be paired with, so
    you can start ignoring the other teams quickly for lightened scouting
    load

--- LazyScout.py
+++ LazyScout.py
@@ -1,36 +1,74 @@
 import csv
+import itertools
+import os
 
 teamofinterest = "613"
 #Be sure to save as a windows csv
 
 teams = []
+lazyScoutsTeamList=[]
+matches = [1]
+maxMatchNumber = 0
 
-def findTeams():
-    with open('Regional.csv') as csvfile2:
-        spamreader = csv.reader(csvfile2, delimiter=',')
+def setMaxNumber(maxNum):
+    maxMatchNumber = maxNum
+
+def deleteOld():
+    os.remove('MatchesIn.csv')
+    os.remove('Teams.csv')
+    os.remove('MatchesToWatch.csv')
+
+def findMatches():
+    global maxMatchNumber
+    i = 0
+    with open('Regional.csv') as sourcefile:
+        spamreader = csv.reader(sourcefile, delimiter=',')
         for row in spamreader:
+            i=i+1
+            maxMatchNumber = i
+            list(row)
+            for j in range (0,len(row)):
+                if row[j] == teamofinterest:
+                    matches.append(i)
+                    with open("MatchesIn.csv", "a") as writefile:
+                            writer = csv.writer(writefile, delimiter=" ")
+                            writer.writerow([teamofinterest + " is in match " + str(row[0])])
+
+def findTeamsInLater():
+    global maxMatchNumber
+    for i in range(0, len(matches)):
+        findTeams(matches[i],maxMatchNumber)
+        lazyScouts(lazyScoutsTeamList,matches[i-1],matches[i])
+
+def findTeams(minMatch,maxMatch):
+    del lazyScoutsTeamList[:]
+    with open('Regional.csv') as sourcefile:
+        spamreader = csv.reader(sourcefile, delimiter=',')
+        for row in itertools.islice(spamreader,minMatch,maxMatch):
             list(row)
             for i in range (0,len(row)):
                 if row[i] == teamofinterest:
                     for i in range (1,len(row)-2):
+                        if row[i] not in lazyScoutsTeamList:
+                            lazyScoutsTeamList.append(row[i])
                         if row[i] not in teams:
                             teams.append(row[i])
                             with open("Teams.csv", "a") as writefile:
                                 writer = csv.writer(writefile, delimiter=",")
-                                writer.writerow([row[i]])       
+                                writer.writerow([row[i]])
 
-def lazyScouts():
-    with open('Regional.csv') as csvfile2:
-        spamreader = csv.reader(csvfile2, delimiter=',')
-        for row in spamreader:
+def lazyScouts(listOfTeams,minMatch,maxMatch):
+    with open('Regional.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        for row in itertools.islice(spamreader,minMatch,maxMatch):
             list(row)
-            for i in range (0,len(teams)):
+            for i in range (0,len(listOfTeams)):
                 for j in range (1,len(row)-2):
-                    if row[j] == teams[i]:
+                    if row[j] == listOfTeams[i]:
                         with open("MatchesToWatch.csv", "a") as writefile2:
                             writer2 = csv.writer(writefile2, delimiter=" ")
-                            writer2.writerow([str(teams[i]) + " in match " + str(row[0])])
+                            if teams[i]!=teamofinterest:
+                                writer2.writerow([str(teams[i]) + " in match " + str(row[0])])
 
-findTeams()
-lazyScouts()
-
+findMatches()
+findTeamsInLater()
